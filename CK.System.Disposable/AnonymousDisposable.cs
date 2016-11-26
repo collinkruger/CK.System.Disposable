cfc7b121b7682aab05d585c78d5e5f4aecc007e4
Collin Kruger <collin.kruger@live.com>
2016-11-26 12:03:38 -0600

    Initial

---
+++ CK.System.Disposable/AnonymousDisposable.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CK.System.Disposable
+{
+    /// <summary>
+    /// Is more or less a box for an <see cref="Action"/>
+    /// </summary>
+    public struct AnonymousDisposable : IDisposable
+    {
+        [DebuggerBrowsable(DebuggerBrowsableState.Never)]
+        volatile Action action;
+
+        /// <summary>
+        /// Indicates if <see cref="Dispose"/> has been invoked (more or less)
+        /// </summary>
+        public bool IsDisposed => action == null;
+
+        
+        /// <summary>
+        /// Constructs an <see cref="AnonymousDisposable"/> that invokes an <see cref="Action"/> on disposal
+        /// </summary>
+        /// <param name="action">The <see cref="Action"/> that is to be invoked when "this" object is disposed</param>
+        public AnonymousDisposable(Action action)
+        {
+            if (action == null)
+                throw new ArgumentNullException(nameof(action));
+
+            this.action = action;
+        }
+
+
+        /// <summary>
+        /// Invokes the underlying <see cref="Action"/> that was passed into the constructor.
+        /// <para>This function is threadsafe, and idempotent; however, if "this" thread dies during execution it is possible for the <see cref="Action"/> not to be invoked.</para>
+        /// </summary>
+        public void Dispose()
+        {
+            var handle = Interlocked.Exchange(ref action, null);
+
+            if (handle != null)
+                action();
+        }
+    }
+}
---
+++ CK.System.Disposable/Disposable.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CK.System.Disposable
+{
+    /// <summary>
+    /// Class for easily generating anonymous <see cref="IDisposable"/>s 
+    /// </summary>
+    public static class Disposable
+    {
+        /// <summary>
+        /// Constructs a new anonymous <see cref="IDisposable"/> that will invoke an <see cref="Action"/> on <see cref="IDisposable.Dispose"/>   
+        /// </summary>
+        /// <param name="action">The action to be invoked on disposal</param>
+        public static IDisposable Gen(Action action) => new AnonymousDisposable(action);
+    }
+}
